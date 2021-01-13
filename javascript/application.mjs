5e9a6546fdfb7c3e9f958f07cfd690e0443844c1
laanatri <laanatri.ade@gmail.com>
2021-01-13 15:27:19 +0100

    retouche menu

--- javascript/application.mjs
+++ javascript/application.mjs
@@ -30,14 +30,28 @@
 
 // console.log("lala");
 
-const eventMenu = () => {
-  const bouton = document.querySelector(".navbar-toggler");
-  bouton.addEventListener('click', () => {
-    console.log("Menu");
-    const nav = document.querySelector(".navbar");
-    setTimeout( () => {
+
+//FOND NAVBAR
+const bouton = document.querySelector(".navbar-toggler");
+
+const fond = () => {
+  setTimeout( () => {
+      const nav = document.querySelector(".navbar");
       nav.classList.toggle("navbar-white");
     }, 100)
+    console.log("fond");
+}
+
+const btnAnim = () => {
+  bouton.classList.toggle("btn-actif");
+  console.log("btnAnim");
+}
+
+const eventMenu = () => {
+  bouton.addEventListener('click', () => {
+    console.log("Menu");
+    fond();
+    btnAnim();
   })
 }
 
