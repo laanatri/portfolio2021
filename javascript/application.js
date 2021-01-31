5ced21ab76365a0d42df6d40a340859c9beb5e24
laanatri <laanatri.ade@gmail.com>
2021-01-31 21:16:44 +0100

    modal genrale

--- javascript/application.js
+++ javascript/application.js
@@ -115,3 +115,59 @@ menu();
 // };
 
 //FIN NAV SCROLLING
+
+
+//MODAL
+
+// Get the modal
+var modal = document.getElementById("myModalSurf");
+var modalContent = document.querySelector(".modal-content");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = () => {
+  modal.style.display = "flex";
+  modal.classList.add("modal-active");
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = () => {
+  modal.style.display = "none";
+  modal.classList.remove("modal-active");
+
+}
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+
+const modale = document.getElementById("myModalSurf");
+console.log(modale);
+
+const btnModal = document.querySelector(".btn-modal");
+const btnClose = document.querySelector(".close");
+
+
+
+
+
+btnModal.addEventListener('click', () => {
+  const scrolll = window.scrollY;
+  const scrollling = () => {
+    window.scrollTo(0, scrolll);
+  };
+  window.addEventListener('scroll', scrollling);
+  btnClose.addEventListener('click', () => {
+    console.log("btn!");
+    window.removeEventListener('scroll', scrollling);
+  })
+})
